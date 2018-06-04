6b1804c8b0c497915caa2a6b949ad56e473fd801
Vlad @Home <158871@novacollege.nl>
2018-06-04 19:27:50 +0200

    fixed that template

--- config.php
+++ config.php
@@ -3,25 +3,25 @@ include "functions.php";
 session_start();
 
 setlocale(LC_ALL, 'nld_nld');
-    // Login configuration
+// Login configuration
 
-    $conf["Username"]= '';
-    $conf["Password"]= '';
-    $conf["Host"]= 'localhost';
-    $conf["Database"]= "dbims";
+$conf["Username"]= 'root';
+$conf["Password"]= '';
+$conf["Host"]= 'localhost';
+$conf["Database"]= "dbims";
 
-    // Connect with the server
-    $mysqli = mysqli_connect($conf["Host"], $conf["Username"], $conf["Password"], $conf["Database"]);
-    $db = new PDO('mysql:dbname=$conf["Database"];host:$conf["Host"];', '$conf["Username"]', '$conf["Password"]');
+// Connect with the server
+$mysqli = mysqli_connect($conf["Host"], $conf["Username"], $conf["Password"], $conf["Database"]);
+$db = new PDO('mysql:dbname=dbims; host=localhost', $conf["Username"], $conf["Password"]);
 
-    // If connection fails
-    if($mysqli == false)
-    {
-        echo "Can't establish connection with the database server";
-    }
+// If connection fails
+if($mysqli == false)
+{
+    echo "Can't establish connection with the database server";
+}
 
-    if($db == false)
-    {
-        echo "Can't establish connection with the database server";
-    }
+if($db == false)
+{
+    echo "Can't establish connection with the database server";
+}
 
