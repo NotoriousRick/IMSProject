3cb0299ea72997e64174c91554282935e7c95ead
Vlad <vpashkov871@student.novacollege.nl>
2018-06-04 19:10:02 +0200

    Use this config fie template, if you are too lazy to make your own

--- config.php
+++ config.php
@@ -3,4 +3,25 @@ include "functions.php";
 session_start();
 
 setlocale(LC_ALL, 'nld_nld');
-    // Hier je Login configuration
+    // Login configuration
+
+    $conf["Username"]= '';
+    $conf["Password"]= '';
+    $conf["Host"]= 'localhost';
+    $conf["Database"]= "dbims";
+
+    // Connect with the server
+    $mysqli = mysqli_connect($conf["Host"], $conf["Username"], $conf["Password"], $conf["Database"]);
+    $db = new PDO('mysql:dbname=$conf["Database"];host:$conf["Host"];', '$conf["Username"]', '$conf["Password"]');
+
+    // If connection fails
+    if($mysqli == false)
+    {
+        echo "Can't establish connection with the database server";
+    }
+
+    if($db == false)
+    {
+        echo "Can't establish connection with the database server";
+    }
+
