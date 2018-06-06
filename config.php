50a10757b9d20f8063099a464895d088ae76727d
Vlad <vpashkov871@student.novacollege.nl>
2018-06-06 09:39:05 +0200

    config file change

--- config.php
+++ config.php
@@ -5,18 +5,23 @@ session_start();
 setlocale(LC_ALL, 'nld_nld');
     // Login configuration
 
-    $conf["Username"]= 'Vlad';
-    $conf["Password"]= 'ihatesushi';
+    $conf["Username"]= '';
+    $conf["Password"]= '';
     $conf["Host"]= 'localhost';
     $conf["Database"]= "dbims";
 
     // Connect with the server
     $mysqli = mysqli_connect($conf["Host"], $conf["Username"], $conf["Password"], $conf["Database"]);
-    $db = new PDO('mysql:dbname=dbims;host:localhost;', 'Vlad', 'ihatesushi');
+    $db = new PDO('mysql:dbname=dbims; host:localhost;', $conf["Username"], $conf["Password"]);
 
     // If connection fails
     if($mysqli == false)
     {
         echo "Can't establish connection with the database server";
     }
+    if ($db == false){
+        echo "nope, something went wrong";
+    }
+	
+		
 
