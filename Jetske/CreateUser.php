6c06977bae109004b236281e57f901f793fbec27
Jetske <jdeboer891@student.novacollege.nl>
2018-06-13 11:16:19 +0200

    update 13 juni files moeten nog gebootstrapt worden, maar werken verder wel

--- Jetske/CreateUser.php
+++ Jetske/CreateUser.php
@@ -30,7 +30,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET')
         if ($stmt->execute() == false)
         {
             //Query mislukt
-            print 'Query mislukt';
+            print 'Oops er is iets misgegaan.';
             exit;
         }
 
@@ -40,83 +40,142 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET')
         //de ingevoerde username bestaat niet er worden dus 0 records gevonden
         if ($result->num_rows === 0)
         {
-            print 'Geen gebruikers gevonden';
+            print 'Geen gebruikers gevonden.';
             exit;
         }
 
         $user = $result->fetch_assoc();
     }
+    elseif (isset($_GET['Delete_ID']))
+    {
+        $sql = "DELETE FROM User WHERE User_ID = ?";
+
+        $stmt = $link->prepare($sql);
+        $stmt->bind_Param("i", $_GET['Delete_ID']);
+
+        //is de $sql query gelukt of niet
+        if ($stmt->execute() == false)
+        {
+            //Query mislukt
+            print 'Oops, er is iets misgegaan.';
+            exit;
+        }
+        header('Location: UserOverzicht.php');
+    }
 }
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST')
 {
-    //als er een user_id bekend is komen we in deze edit/update functie en vullen we dat bij behorende username alvast in
-    if (isset($_GET['User_ID']))
-    {
-        $username = $_POST['UserName'];
-        $isadmin = isset($_POST['IsAdmin']) ? true : false;
+    $username = $_POST['UserName'];
+    $pwd = $_POST['Password'];
+    $PwdFouten = "";
 
-        //als er geen nieuw wachtwoord wordt ingesteld dan blijft het oude wachtwoord in de database staan 
-        if ($_POST['Password'] !== 'niet het wachtwoord')
+    //in deze sring lengte if-statement controleren we server side of er een username is gezet/ingevuld
+    if (strlen($username) === 0)
+    {
+        $GeenUsername = true;
+    }
+
+    //in deze string lengte if-statement controleren we server side of het password aan de eis voldoet
+    if (strlen($pwd) <= 8)
+    {
+        $GeenPassword = true;
+        $PwdFouten = "Minstens 8 karakters";
+    }
+
+    //in de onderstaande 4 preg_match if-statements controleren we server side of het password aan die 4 eisen voldoet
+    if (preg_match("([!,%,&,@,#,$,^,*,?,_,~])", $pwd) === 0)
+    {
+        $GeenPassword = true;
+        $PwdFouten = $PwdFouten . "<br>" . "Uw invoer dient minimaal 1 speciaal teken te bevaten";
+    }
+
+    if (preg_match("([a-z])", $pwd) === 0)
+    {
+        $GeenPassword = true;
+        $PwdFouten = $PwdFouten . "<br>" . "Uw invoer dient minimaal 1 kleine letter";
+    }
+    
+    if (preg_match("([A-Z])", $pwd) === 0)
+    {
+        $GeenPassword = true;
+        $PwdFouten = $PwdFouten . "<br>" . "Uw invoer dient minimaal 1 hoofdletter";
+    }
+    
+    if (preg_match("([0-9])", $pwd) === 0)
+    {
+        $GeenPassword = true;
+        $PwdFouten = $PwdFouten . "<br>" . "Uw invoer dient minimaal 1 getal";
+    }
+
+
+    if (!isset($GeenUsername) && !isset($GeenPassword))
+    {
+        //als er een user_id bekend is komen we in deze edit/update functie en vullen we dat bij behorende username alvast in
+        if (isset($_GET['User_ID']))
+        {
+            $isadmin = isset($_POST['IsAdmin']) ? true : false;
+
+            //als er geen nieuw wachtwoord wordt ingesteld dan blijft het oude wachtwoord in de database staan 
+            if ($_POST['Password'] !== 'niet het wachtwoord')
+            {
+                $password = password_hash($_POST['Password'], PASSWORD_BCRYPT);
+            }
+
+            $sql = "UPDATE User SET UserName = ?, IsAdmin = ?";
+            //als er wel een nieuw wachtwoord wordt ingesteld dan wordt de passwordhash geupdate
+            if (isset($password))
+            {
+                $sql = $sql . ", PasswordHash = ?";
+            }
+            $sql = $sql . " WHERE User_ID = ?";
+
+            $stmt = $link->prepare($sql);
+
+            //als er wel een nieuw wachtwoord wordt ingesteld dan wordt de passwordhash geupdate
+            if (isset($password))
+            {
+                $stmt->bind_param("sisi", $username, $isadmin, $password, $_GET['User_ID']);
+            }
+            else //als er geen nieuw wachtwoord wordt ingesteld
+            {
+                $stmt->bind_param("sii", $username, $isadmin, $_GET['User_ID']);
+            }
+
+            print($sql);
+
+            //is de $sql query voor het updaten gelukt of niet
+            if ($stmt->execute() === true)
+            {
+                //Query gelukt
+                header('Location: UserOverzicht.php');
+            }
+            else
+            {
+                print $link->error;
+            }
+        }
+        else // als er geen user_id bekend is komen we in deze create user functie
         {
             $password = password_hash($_POST['Password'], PASSWORD_BCRYPT);
-        }
 
-        $sql = "UPDATE User SET UserName = ?, IsAdmin = ?";
-        //als er wel een nieuw wachtwoord wordt ingesteld dan wordt de passwordhash geupdate
-        if (isset($password))
-        {
-            $sql = $sql . ", PasswordHash = ?";
-        }
-        $sql = $sql . " WHERE User_ID = ?";
+            $sql = "INSERT INTO User (UserName, PasswordHash) Values (?, ?)";
 
-        $stmt = $link->prepare($sql);
+            $stmt = $link->prepare($sql);
+            $stmt->bind_param("ss", $username, $password);
 
-        //als er wel een nieuw wachtwoord wordt ingesteld dan wordt de passwordhash geupdate
-        if (isset($password))
-        {
-            $stmt->bind_param("sisi", $username, $isadmin, $password, $_GET['User_ID']);
+            if ($stmt->execute())
+            {
+                //Query gelukt
+                header('Location: UserOverzicht.php');
+            }
+            else
+            {
+                print $link->error;
+            }
         }
-        else //als er geen nieuw wachtwoord wordt ingesteld
-        {
-            $stmt->bind_param("sii", $username, $isadmin, $_GET['User_ID']);
-        }
-
-        print($sql);
-        
-        //is de $sql query voor het updaten gelukt of niet
-        if ($stmt->execute() === true)
-        {
-            //Query gelukt
-            header('Location: UserOverzicht.php');
-        }
-        else
-        {
-            print $link->error;
-        }
+        $link->close();
     }
-    else // als er geen user_id bekend is komen we in deze create user functie
-    {
-
-        $username = $_POST['UserName'];
-        $password = password_hash($_POST['Password'], PASSWORD_BCRYPT);
-
-        $sql = "INSERT INTO User (UserName, PasswordHash) Values (?, ?)";
-
-        $stmt = $link->prepare($sql);
-        $stmt->bind_param("ss", $username, $password);
-
-        if ($stmt->execute())
-        {
-            //Query gelukt
-            header('Location: UserOverzicht.php');
-        }
-        else
-        {
-            print $link->error;
-        }
-    }
-    $link->close();
 }
 ?>
 
@@ -132,22 +191,17 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST')
             <div>
                 <label>Gebruikersnaam: </label><br>
                 <input type="text" name="UserName" value="<?php if (isset($user)) print $user['UserName']; ?>" required/><br><br>
+                <?php if (isset($GeenUsername)) print '<span style="color:red">Voer alstublieft een username in.</span><br>'; ?>
                 <label>Wachtwoord: </label><br>
                 <input type="password" name="Password" value="<?php if (isset($user)) print 'niet het wachtwoord' ?>" required/><br><br>
+                <?php if (isset($GeenPassword)) print '<span style="color:red">' . $PwdFouten . '</span><br>'; ?>
                 <label>Is Admin?</label>
                 <input type="checkbox" name="IsAdmin" <?php if (isset($user) && $user['IsAdmin'] == 1) print 'checked="true"' ?>/>
                 <input type="submit" value="opslaan">
+                <a href="UserOverzicht.php">annuleren</a>
             </div>
         </form>
     </body>
-
-    <script>
-        $(document).ready(function()
-        {
-            $( ":password" ).PassRequirements();
-        });
-    </script>
-
     <!--Main jQuery library-->
     <script src="../Scripts/jquery-3.3.1.slim.js"></script>
 
@@ -159,4 +213,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST')
 
     <!--Jetske's special script-->
     <script src="../Scripts/PassRequirements.js"></script>
+    
+    
+    <script>
+        $(document).ready(function()
+        {
+            $( ":password" ).PassRequirements();
+        });
+    </script>
 </html>
