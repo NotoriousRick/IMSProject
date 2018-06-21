1b65e3a3c546d465289b3759a282be76f2cff834
Jetske <jdeboer891@student.novacollege.nl>
2018-06-21 10:46:25 +0200

    Update js script

--- Scripts/PassRequirements.js
+++ Scripts/PassRequirements.js
@@ -1,5 +1,5 @@
 if (typeof jQuery === 'undefined') {
-  throw new Error('PassRequirements requires jQuery')
+    throw new Error('PassRequirements requires jQuery')
 }
 
 +(function ($) {
@@ -9,13 +9,13 @@ if (typeof jQuery === 'undefined') {
         var defaults = {
 //            defaults: true
         };
-        
-        if(
-            !options ||                     //if no options are passed                                  /*
-            options.defaults == true ||     //if default option is passed with defaults set to true      * Extend options with default ones
-            options.defaults == undefined   //if options are passed but defaults is not passed           */
-        ){
-            if(!options){                   //if no options are passed, 
+
+        if (
+                !options || //if no options are passed                                  /*
+                options.defaults == true || //if default option is passed with defaults set to true      * Extend options with default ones
+                options.defaults == undefined   //if options are passed but defaults is not passed           */
+                ) {
+            if (!options) {                   //if no options are passed, 
                 options = {};               //create an options object
             }
             defaults.rules = $.extend({
@@ -44,21 +44,21 @@ if (typeof jQuery === 'undefined') {
                     regex: new RegExp('[^0-9]', 'g')
                 }
             }, options.rules);
-        }else{
+        } else {
             defaults = options;     //if options are passed with defaults === false
         }
-        
 
-        
+
+
         var i = 0;
 
         return this.each(function () {
-            
-            if(!defaults.defaults && !defaults.rules){
+
+            if (!defaults.defaults && !defaults.rules) {
                 console.error('You must pass in your rules if defaults is set to false. Skipping this input with id:[' + this.id + '] with class:[' + this.classList + ']');
                 return false;
             }
-            
+
             var requirementList = "";
             $(this).data('pass-req-id', i++);
 
@@ -66,27 +66,43 @@ if (typeof jQuery === 'undefined') {
                 var this_ = $(this);
                 Object.getOwnPropertyNames(defaults.rules).forEach(function (val, idx, array) {
                     if (this_.val().replace(defaults.rules[val].regex, "").length > defaults.rules[val].minLength - 1) {
-                        $('#' + val).css('text-decoration','line-through');
+                        $('#' + val).css('text-decoration', 'line-through');
                     } else {
-                        $('#' + val).css('text-decoration','none');
+                        $('#' + val).css('text-decoration', 'none');
                     }
 
                 })
+
+                try
+                {
+                    Object.getOwnPropertyNames(defaults.rules).forEach(function (val, idx, array) {
+                        if (this_.val().replace(defaults.rules[val].regex, "").length > defaults.rules[val].minLength - 1) {
+                            $(this_).data('password-valid', true);
+                        } else {
+                            $(this_).data('password-valid', false);
+                            throw BreakException;
+                        }
+
+                    })
+                } catch (e)
+                {
+
+                }
             });
 
             Object.getOwnPropertyNames(defaults.rules).forEach(function (val, idx, array) {
                 requirementList += (("<li id='" + val + "'>" + defaults.rules[val].text).replace("minLength", defaults.rules[val].minLength));
             })
-            try{
-            $(this).popover({
-                title: 'Wachtwoord eisen',
-                trigger: options.trigger ? options.trigger : 'focus',
-                html: true,
-                placement: options.popoverPlacement ? options.popoverPlacement : 'right',
-                content: 'Uw wachtwoord moet:<ul>' + requirementList + '</ul>'
-                    //                        '<p>The confirm field is actived only if all criteria are met</p>'
-            });
-            }catch(e){
+            try {
+                $(this).popover({
+                    title: 'Wachtwoord eisen',
+                    trigger: options.trigger ? options.trigger : 'focus',
+                    html: true,
+                    placement: options.popoverPlacement ? options.popoverPlacement : 'right',
+                    content: 'Uw wachtwoord moet:<ul>' + requirementList + '</ul>'
+                            //                        '<p>The confirm field is actived only if all criteria are met</p>'
+                });
+            } catch (e) {
                 throw new Error('PassRequirements requires Bootstraps Popover plugin');
             }
             $(this).focus(function () {
